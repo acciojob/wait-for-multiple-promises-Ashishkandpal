cb67c4619f6e7ac914510eba888596af56e600f7
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-05-17 17:39:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
 // Create three promises
 const randomNumberGenerator = function () {
-  return (Math.random() * (3 - 1 + 1) + 1).toFixed(3);
+  return (Math.random() * (3 - 1 + 1) + 1).toFixed(4);
 };
 
 const timeout = function (ms) {
